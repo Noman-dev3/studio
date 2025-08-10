269631171c0d5e0918e3449a37d2ab95ab8149f7
Noman khan <noman.dev3@gmail.com>
2025-08-10 22:18:37 +0500

    Update piiss-logo.tsx

--- src/components/icons/piiss-logo.tsx
+++ src/components/icons/piiss-logo.tsx
@@ -3,11 +3,14 @@
 import * as React from 'react';
 import { Skeleton } from '../ui/skeleton';
 
-interface PiissLogoProps extends React.SVGProps<SVGSVGElement> {
+// Extract SVG props from all possible props to avoid passing them to the Skeleton
+type SVGProps = React.SVGProps<SVGSVGElement>;
+
+interface PiissLogoProps extends SVGProps {
     schoolName?: string;
 }
 
-export function PiissLogo({ schoolName, ...props }: PiissLogoProps) {
+export function PiissLogo({ schoolName, className, ...props }: PiissLogoProps) {
   const [displayName, setDisplayName] = React.useState('PIISS');
   
   React.useEffect(() => {
@@ -18,14 +21,21 @@ export function PiissLogo({ schoolName, ...props }: PiissLogoProps) {
   }, [schoolName]);
 
   if (!schoolName) {
-    return <Skeleton className="h-8 w-20" {...props} />;
+    // Only pass className to the Skeleton component, not all SVG props
+    return <Skeleton className={cn("h-8 w-20", className)} />;
   }
 
   return (
-    <svg width="100" height="32" viewBox="0 0 100 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg width="100" height="32" viewBox="0 0 100 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className={className} {...props}>
       <text x="0" y="24" fontFamily="'PT Sans', sans-serif" fontSize="24" fontWeight="bold" className="text-primary dark:text-primary-foreground">
         {displayName}
       </text>
     </svg>
   );
 }
+
+// Helper function to combine class names
+function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
